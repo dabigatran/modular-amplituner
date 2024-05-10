835afa3c2c08b5816546da54c174b9855cdc5386
dabigatran <44303797+dabigatran@users.noreply.github.com>
2024-05-10 18:04:51 +0200

    Update spi.c

--- main/spi.c
+++ main/spi.c
@@ -39,12 +39,12 @@ esp_err_t spi_init2(void){
         .address_bits=0,
         .dummy_bits=0,
         .clock_speed_hz=400000,
-        .duty_cycle_pos=128,        //50% duty cycle
+        .duty_cycle_pos=128, 
         .mode=2,
         .spics_io_num=GPIO_CS,
-        .cs_ena_pretrans=1,        //Keep the CS low 3 cycles after transaction, to stop slave from missing the last bit when CS has less propagation delay than CLK
-        .cs_ena_posttrans=1,        //Keep the CS low 3 cycles after transaction, to stop slave from missing the last bit when CS has less propagation delay than CLK
-        .queue_size=10,  //Specify pre-transfer callback to handle D/C line
+        .cs_ena_pretrans=1,
+        .cs_ena_posttrans=1, 
+        .queue_size=10,
     };
     
    esp_err_t ret= spi_bus_initialize(SENDER_HOST, &bus_config, SPI_DMA_CH_AUTO);
